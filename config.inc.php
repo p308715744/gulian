e6f43fc2c28e8087c56f8cfae8c8e0f5f56751fb
unknown <20111224i705@20111224i705-PC.(none)>
2012-10-24 15:47:14 +0800

    Signed-off-by: unknown <20111224i705@20111224i705-PC.(none)>

---
+++ config.inc.php
@@ -0,0 +1,14 @@
+<?php
+    if (!defined('IN_ET')) exit();
+
+    return array(
+        'DB_TYPE'=>'mysql',
+        'DB_HOST'=>'localhost',
+        'DB_NAME'=>'gulian',
+        'DB_USER'=>'root',
+        'DB_PWD'=>'',
+        'DB_PORT'=>3306,
+        'DB_PREFIX'=>'',
+        'APP_DEBUG'=>false,
+    );
+    ?>
